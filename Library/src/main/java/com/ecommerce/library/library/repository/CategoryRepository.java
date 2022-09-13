72d817b7b0a23c358f0a9ebde38b242d6e7aa425
Sahak-S <sahak.sa@mail.ru>
2022-09-13 23:03:54 +0400

    Add model

---
+++ Library/src/main/java/com/ecommerce/library/library/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.library.library.repository;
+
+import com.ecommerce.library.library.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
---
+++ Library/src/main/java/com/ecommerce/library/library/service/CategoryService.java
@@ -0,0 +1,26 @@
+package com.ecommerce.library.library.service;
+
+import com.ecommerce.library.library.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    /*Admin*/
+    List<Category> findAll();
+    Category save(Category category);
+    Category getById(Long id);
+    Category update(Category category);
+    void deleteById(Long id);
+    void enabledById(Long id);
+
+
+//    Category findById(Long id);
+//    Category update(Category category);
+//
+//
+//    List<Category> findAllByActivated();
+
+//    /*Customer*/
+//    List<CategoryDto> getCategoryAndProduct();
+}
